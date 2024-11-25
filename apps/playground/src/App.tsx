5f0e7521715b65462ff08d09f618052a47f45680
spiderman <spiderman@Michaels-MacBook-Pro.local>
2024-11-25 15:50:54 +0200

    npmignore prettierrc

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,35 +1,35 @@
-import './App.css';
-import { useRemultForm } from 'remult-uikit';
-import type { SubmitData } from 'remult-uikit';
-import { User } from './shared/User';
-import { remult } from 'remult';
+import './App.css'
+import { useRemultForm } from 'remult-uikit'
+import type { SubmitData } from 'remult-uikit'
+import { User } from './shared/User'
+import { remult } from 'remult'
 
-const userRepo = remult.repo(User);
+const userRepo = remult.repo(User)
 
 function App() {
-  const { subscribe, handleSubmit, errors } = useRemultForm(userRepo);
+	const { subscribe, handleSubmit, errors } = useRemultForm(userRepo)
 
-  const onSubmit = (data: SubmitData<User>) => {
-    console.log('data', data);
-  };
+	const onSubmit = (data: SubmitData<User>) => {
+		console.log('data', data)
+	}
 
-  return (
-    <form
-      onSubmit={(e) => handleSubmit(e, onSubmit)}
-      style={{ display: 'flex', flexDirection: 'column' }}
-    >
-      <span>{userRepo.fields.email.caption}</span>
-      <input {...subscribe('email')} />
-      {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
-      <span>{userRepo.fields.firstName.caption}</span>
-      <input {...subscribe('firstName')} />
-      {errors.firstName && <p style={{ color: 'red' }}>{errors.firstName}</p>}
-      <span>{userRepo.fields.age.caption}</span>
-      <input {...subscribe('age')} />
-      {errors.age && <p style={{ color: 'red' }}>{errors.age}</p>}
-      <button>Submit</button>
-    </form>
-  );
+	return (
+		<form
+			onSubmit={(e) => handleSubmit(e, onSubmit)}
+			style={{ display: 'flex', flexDirection: 'column' }}
+		>
+			<span>{userRepo.fields.email.caption}</span>
+			<input {...subscribe('email')} />
+			{errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
+			<span>{userRepo.fields.firstName.caption}</span>
+			<input {...subscribe('firstName')} />
+			{errors.firstName && <p style={{ color: 'red' }}>{errors.firstName}</p>}
+			<span>{userRepo.fields.age.caption}</span>
+			<input {...subscribe('age')} />
+			{errors.age && <p style={{ color: 'red' }}>{errors.age}</p>}
+			<button>Submit</button>
+		</form>
+	)
 }
 
-export default App;
+export default App
--- packages/remult-uikit/src/index.ts
+++ packages/remult-uikit/src/index.ts
@@ -1,64 +1,64 @@
-import { FormEvent, useState } from 'react';
-import { Repository } from 'remult';
+import { FormEvent, useState } from 'react'
+import { Repository } from 'remult'
 
 export type SubmitData<T> = {
-  [k in keyof T]?: any;
-};
+	[k in keyof T]?: any
+}
 
 type FormError<T> = {
-  [k in keyof T]?: string;
-};
+	[k in keyof T]?: string
+}
 
 export const useRemultForm = <T>(repo: Repository<T>) => {
-  // const [elements, setElements] = useState<T>({} as T);
-  const [errors, setErrors] = useState<FormError<T>>({});
+	// const [elements, setElements] = useState<T>({} as T);
+	const [errors, setErrors] = useState<FormError<T>>({})
 
-  const subscribe = (fieldId: keyof T) => {
-    return {
-      // onChange: (event: ChangeEvent<HTMLInputElement>) => {
-      //   setElements({ ...elements, [fieldId]: event.target.value });
-      // },
-      name: fieldId,
-      type: repo.fields[fieldId].inputType,
-    };
-  };
+	const subscribe = (fieldId: keyof T) => {
+		return {
+			// onChange: (event: ChangeEvent<HTMLInputElement>) => {
+			//   setElements({ ...elements, [fieldId]: event.target.value });
+			// },
+			name: fieldId,
+			type: repo.fields[fieldId].inputType,
+		}
+	}
 
-  const handleSubmit = async (
-    e: FormEvent<HTMLFormElement>,
-    onSuccess: (data: SubmitData<T>) => void
-  ) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const dataObject = Object.fromEntries(formData.entries()); // Convert FormData to an object
-    let isValid = true;
-    setErrors({});
-    const newErrors: FormError<T> = {};
+	const handleSubmit = async (
+		e: FormEvent<HTMLFormElement>,
+		onSuccess: (data: SubmitData<T>) => void
+	) => {
+		e.preventDefault()
+		const formData = new FormData(e.currentTarget)
+		const dataObject = Object.fromEntries(formData.entries()) // Convert FormData to an object
+		let isValid = true
+		setErrors({})
+		const newErrors: FormError<T> = {}
 
-    for (const field in dataObject) {
-      // const fieldState = getFieldState(
-      //   dataObject[field],
-      //   // @ts-ignore TODO: fix type error
-      //   repo.fields[field as keyof T]
-      // );
-      // if (fieldState) {
-      //   setErrors({ ...errors, ...fieldState });
-      //   isValid = false;
-      // }
-      // @ts-ignore TODO: fix type error
-      const error = await repo.validate(dataObject, field);
-      if (error) {
-        // @ts-ignore TODO: fix type error
-        newErrors[field] = error.message;
-        isValid = false;
-      }
-    }
+		for (const field in dataObject) {
+			// const fieldState = getFieldState(
+			//   dataObject[field],
+			//   // @ts-ignore TODO: fix type error
+			//   repo.fields[field as keyof T]
+			// );
+			// if (fieldState) {
+			//   setErrors({ ...errors, ...fieldState });
+			//   isValid = false;
+			// }
+			// @ts-ignore TODO: fix type error
+			const error = await repo.validate(dataObject, field)
+			if (error) {
+				// @ts-ignore TODO: fix type error
+				newErrors[field] = error.message
+				isValid = false
+			}
+		}
 
-    if (isValid) {
-      onSuccess(dataObject as SubmitData<T>);
-    } else {
-      setErrors(newErrors);
-    }
-  };
+		if (isValid) {
+			onSuccess(dataObject as SubmitData<T>)
+		} else {
+			setErrors(newErrors)
+		}
+	}
 
-  return { subscribe, handleSubmit, errors };
-};
+	return { subscribe, handleSubmit, errors }
+}
