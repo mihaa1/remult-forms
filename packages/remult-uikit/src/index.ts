d318b6aa57ea91db3c6ae1ac2ca67bceccc93502
spiderman <spiderman@Michaels-MacBook-Pro.local>
2024-11-26 21:00:43 +0200

    test regular publish

--- packages/remult-uikit/src/index.ts
+++ packages/remult-uikit/src/index.ts
@@ -10,7 +10,7 @@ type FormError<T> = {
 }
 
 export const useRemultForm = <T>(repo: Repository<T>) => {
-	console.log('TEST PUBLISH', 210)
+	console.log('TEST PUBLISH', 211)
 	// const [elements, setElements] = useState<T>({} as T);
 	const [errors, setErrors] = useState<FormError<T>>({})
 
